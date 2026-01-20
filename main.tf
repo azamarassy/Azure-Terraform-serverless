bfcf433e3777f6a88781510724826f9afac524c2
owner <kakuma3090@gmail.com>
2026-01-20 21:20:33 +0900

    Translate comments in main.tf to Japanese

--- main.tf
+++ main.tf
@@ -1,87 +1,88 @@
 # main.tf
-# This file serves as the entry point for the Terraform configuration.
-# It defines the required Terraform providers, configures the Azure provider,
-# and sets up the main resource group and the backend for storing Terraform state.
+# このファイルは、Terraform 構成のエントリポイントとして機能します。
+# 必要な Terraform プロバイダーを定義し、Azure プロバイダーを構成し、
+# メインのリソースグループと Terraform ステートを保存するためのバックエンドを設定します。
 
-# Terraform Configuration Block
-# Specifies the required Terraform version and providers.
+# Terraform 構成ブロック
+# 必要な Terraform バージョンとプロバイダーを指定します。
 terraform {
-  # Defines the required providers and their versions.
+  # 必要なプロバイダーとそのバージョンを定義します。
   required_providers {
-    # Specifies the AzureRM provider.
+    # AzureRM プロバイダーを指定します。
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0" # Constrains the provider version to avoid breaking changes.
+      version = "~> 3.0" # 互換性のない変更を避けるためにプロバイダーのバージョンを制約します。
     }
   }
 
-  # Backend Configuration for storing Terraform state in Azure Storage.
-  # This block is commented out initially. It should be uncommented and configured
-  # AFTER the Azure Storage Account and Container for state are created.
-  # Backend configuration cannot use Terraform expressions because it needs to be
-  # known before Terraform initializes and evaluates expressions.
+  # Azure Storage に Terraform ステートを保存するためのバックエンド構成。
+  # このブロックは最初コメントアウトされています。
+  # tfstate 用の Azure Storage アカウントとコンテナが作成された後、
+  # コメントアウトを解除して構成する必要があります。
+  # バックエンド構成は、Terraform が初期化されて式を評価する前に認識されている必要があるため、
+  # Terraform 式を使用できません。
   /*
   backend "azurerm" {
-    # The name of the resource group where the tfstate storage account is located.
+    # tfstate ストレージアカウントが配置されているリソースグループの名前。
     resource_group_name  = "your-tfstate-resource-group-name"
-    # The name of the Azure Storage Account used for storing Terraform state.
+    # Terraform ステートを保存するために使用される Azure ストレージアカウントの名前。
     storage_account_name = "yourtfstatestorageaccountname"
-    # The name of the container within the storage account to store the tfstate file.
+    # tfstate ファイルを保存するコンテナの名前。
     container_name       = "tfstate"
-    # The path to the tfstate file within the container.
+    # コンテナ内の tfstate ファイルへのパス。
     key                  = "terraform.tfstate"
   }
   */
 }
 
-# Azure Provider Configuration
-# Configures the AzureRM provider with default settings.
+# Azure プロバイダー構成
+# AzureRM プロバイダーをデフォルト設定で構成します。
 provider "azurerm" {
-  # The 'features' block is required but can be empty for default settings.
+  # 'features' ブロックは必須ですが、デフォルト設定の場合は空にできます。
   features {}
 }
 
-# Main Resource Group
-# Defines the primary Azure Resource Group where all other resources will be deployed.
+# メインのリソースグループ
+# 他のすべてのリソースがデプロイされるプライマリ Azure リソースグループを定義します。
 resource "azurerm_resource_group" "main" {
-  # The name of the resource group.
+  # リソースグループの名前。
   name     = var.resource_group_name
-  # The Azure region where the resource group will be located.
+  # リソースグループが配置される Azure リージョン。
   location = var.location
 }
 
-# Resources for Terraform state backend
-# These resources are for setting up the Azure Storage Account and Container
-# to store the Terraform state file remotely.
+# Terraform ステートバックエンド用のリソース
+# これらのリソースは、Terraform ステートファイルをリモートに保存するための
+# Azure ストレージアカウントとコンテナを設定するためのものです。
 
-# Generates a random suffix to ensure the storage account name is globally unique.
+# ストレージアカウント名がグローバルで一意であることを保証するために、ランダムなサフィックスを生成します。
 resource "random_id" "tfstate_suffix" {
-  byte_length = 8 # Generates 8 random bytes, which results in 16 hex characters.
+  byte_length = 8 # 8 バイトのランダムなバイトを生成し、16 進数で 16 文字になります。
 }
 
-# Storage Account for Terraform state
+# Terraform ステート用ストレージアカウント
 resource "azurerm_storage_account" "tfstate" {
-  # Name of the storage account, made unique with a random suffix.
-  name                     = "tfstate${random_id.tfstate_suffix.hex}" # Unique name
-  # The resource group where the storage account will be deployed.
+  # ランダムなサフィックスで一意化されたストレージアカウントの名前。
+  name                     = "tfstate${random_id.tfstate_suffix.hex}" # 一意の名前
+  # ストレージアカウントがデプロイされるリソースグループ。
   resource_group_name      = azurerm_resource_group.main.name
-  # The Azure region where the storage account will be deployed.
+  # ストレージアカウントがデプロイされる Azure リージョン。
   location                 = azurerm_resource_group.main.location
-  # Tier of the storage account (e.g., "Standard", "Premium").
+  # ストレージアカウントのティア (例: "Standard", "Premium")。
   account_tier             = "Standard"
-  # Replication type for the storage account (e.g., "LRS", "GRS", "RA-GRS").
-  # GRS (Geo-Redundant Storage) provides high durability by replicating data to a secondary region.
-  account_replication_type = "GRS" # Geo-Redundant Storage for high durability
-  # Minimum TLS version required for connections to the storage account.
+  # ストレージアカウントのレプリケーションタイプ (例: "LRS", "GRS", "RA-GRS")。
+  # GRS (Geo-Redundant Storage) は、データをセカンダリリージョンにレプリケートすることで高い耐久性を提供します。
+  account_replication_type = "GRS" # 高い耐久性のための地理冗長ストレージ
+  # ストレージアカウントへの接続に必要とされる最小 TLS バージョン。
   min_tls_version          = "TLS1_2"
 }
 
-# Storage Container for Terraform state
+# Terraform ステート用ストレージコンテナ
 resource "azurerm_storage_container" "tfstate" {
-  # Name of the container where the Terraform state file will be stored.
+  # Terraform ステートファイルが保存されるコンテナの名前。
   name                  = "tfstate"
-  # The storage account where this container will be created.
+  # このコンテナが作成されるストレージアカウント。
   storage_account_name  = azurerm_storage_account.tfstate.name
-  # Access type for the container (e.g., "private", "blob", "container").
+  # コンテナのアクセスタイプ (例: "private", "blob", "container")。
   container_access_type = "private"
 }
